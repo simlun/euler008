ed0f0083e2f56d7885b1a5172e195f20d089786b
frerom <roman.fredrik@gmail.com>
2012-12-08 21:11:56 +0000

    Finds greatest 5 factored product

--- src/euler008/core.clj
+++ src/euler008/core.clj
@@ -3,8 +3,19 @@
 (defn to-integer-list [input]
   (map #(read-string (str %)) input))
 
-(defn find-greatest-product [])
+(defn- inner2 [five-factors greatest-product]
+  (let [product (reduce * five-factors)]
+    (if (> product greatest-product) product greatest-product)))
+
+(defn- inner [numbers greatest-product]
+  (let [next-numbers (rest numbers)
+        five-factors (take 5 numbers)]
+    (cond 
+      (< (count numbers) 5) greatest-product
+      :else (inner next-numbers (inner2 five-factors greatest-product)))))
+
+(defn find-greatest-5product [numbers]
+  (inner numbers 0))
 
 (defn mjao [input]
-  (find-greatest-product (to-integer-list input))
-  )
+  (find-greatest-5product (to-integer-list input)))
