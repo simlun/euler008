6a62038bb660c2f0364a61a49c6734f71e99513a
Simon Lundmark <simon.lundmark@gmail.com>
2012-12-08 23:29:51 +0000

    Refactoring

--- src/euler008/core.clj
+++ src/euler008/core.clj
@@ -1,25 +1,29 @@
 (ns euler008.core)
 
-(defn to-integer-list [input]
-  (map #(read-string (str %)) input))
-
-(defn- inner2 [five-factors greatest-product]
-  (let [product (reduce * five-factors)]
-    (if (> product greatest-product) product greatest-product)))
-
-(defn- inner [numbers greatest-product]
-  (let [next-numbers (rest numbers)
-        five-factors (take 5 numbers)]
-    (cond 
-      (< (count numbers) 5) greatest-product
-      :else (inner next-numbers (inner2 five-factors greatest-product)))))
+(defn- greatest [a b]
+  (if (> a b)
+    a
+    b))
 
 (defn find-greatest-5product [numbers]
-  (inner numbers 0))
+  (defn- recursion [numbers current-max]
+    (let [next-numbers (rest numbers)
+          number-count (count numbers)
+          five-numbers (take 5 numbers)
+          done (< number-count 5)
+          product (reduce * five-numbers)
+          greatest (greatest product current-max)]
+      (if done
+        current-max
+        (recursion next-numbers greatest))))
+  (recursion numbers 0))
 
-(defn mjao [input]
-  (find-greatest-5product (to-integer-list input)))
+(defn string-to-integer-list [input]
+  (map #(read-string (str %)) input))
+
+(defn find-greatest-5product-from-string [input]
+  (find-greatest-5product (string-to-integer-list input)))
 
 (defn -main [& args]
-  (println (mjao (read-line))))
+  (println (find-greatest-5product-from-string (read-line))))
 
--- test/euler008/core_test.clj
+++ test/euler008/core_test.clj
@@ -5,18 +5,23 @@
 (fact "midje seems to work"
   (+ 1 2) => 3)
 
+
 (fact
-  (mjao "12345") => 120
+  (find-greatest-5product-from-string "12345") => 120
   (provided
+    (string-to-integer-list "12345") => '(1 2 3 4 5)
     (find-greatest-5product '(1 2 3 4 5)) => 120))
 
 (fact
-  (mjao "23456") => 720
+  (find-greatest-5product-from-string "23456") => 720
   (provided
+    (string-to-integer-list "23456") => '(2 3 4 5 6)
     (find-greatest-5product '(2 3 4 5 6)) => 720))
 
+
 (fact
-   (to-integer-list "12345") => '(1 2 3 4 5))
+   (string-to-integer-list "12345") => '(1 2 3 4 5))
+
 
 (fact
   (find-greatest-5product '(1 2 3 4 5)) => 120
@@ -26,7 +31,6 @@
   (find-greatest-5product '(1 2 3 4 5 6)) => 720)
 
 (fact
-  (find-greatest-5product '(1 2 3 4 5 6 7)) => 2520)
-
-(fact
+  (find-greatest-5product '(1 2 3 4 5 6 7)) => 2520
   (find-greatest-5product '(1 2 3 4 5 6 1)) => 720)
+
